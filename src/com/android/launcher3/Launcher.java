8a50a192f10bc6e77692b9e65221d954723e400a
ezio84 <brabus84@gmail.com>
2018-11-11 19:25:44 +0100

    Launcher drawer icon pack: use Recents icons engine

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -2490,6 +2490,7 @@ public class Launcher extends BaseDraggingActivity implements LauncherExterns,
         }
         if ("pref_iconPackPackage".equals(key)) {
             mModel.clearIconCache();
+            mModel.setIconPack();
             mModel.forceReload();
         }
         if (KEY_HOMESCREEN_DT_GESTURES.equals(key)) {
